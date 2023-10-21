b0f830918ca2935906b2e4d659c006dc2bc7af47
myClones <145363574+nova-x0@users.noreply.github.com>
2023-10-21 14:45:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-
-
-
-  // nav bar animation
+// nav bar animation
 
 let isToggled = false;
 const menuButton = document.querySelector(".nav-menu");
@@ -10,7 +7,7 @@ const menuItems = document.querySelectorAll(".menu-list");
 
 menuButton.addEventListener("click", toggleMenu);
 
-menuItems.forEach(item => {
+menuItems.forEach((item) => {
   item.addEventListener("click", toggleMenu);
 });
 
@@ -23,20 +20,19 @@ function toggleMenu() {
     menuUl.style.animationName = "wipe-out-up";
     menuUl.style.animationDuration = "0.5s";
 
-
-    setTimeout(() => {     // Delay hiding the menu to allow the animation to complete
+    setTimeout(() => {
+      // Delay hiding the menu to allow the animation to complete
       menuUl.style.display = "none";
     }, 500);
   }
   isToggled = !isToggled;
 }
 
-
 // auto typing text code
 const textElement = document.getElementById("auto-typing-text");
 const names = [
   "Web Developer.    ",
-  "Web Designer.   ",    // Your list of names
+  "Web Designer.   ", // Your list of names
   "Graphic Designer.   ",
 ];
 const typingSpeed = 200; // Adjust the typing speed (milliseconds per character)
@@ -71,51 +67,27 @@ function type() {
   setTimeout(type, speed);
 }
 
-
-window.onload = type;  // Start the typing effect when the page loads
+window.onload = type; // Start the typing effect when the page loads
 
 //gsap animation
 
-const splitScreenAnimation = document.querySelector(".home-splitting")
-const sectionTwoAnimation = document.querySelector(".section-2-pic-nd-about")
-
+const splitScreenAnimation = document.querySelector(".home-splitting");
+const sectionTwoAnimation = document.querySelector(".section-2-pic-nd-about");
 
 gsap.from(splitScreenAnimation, {
-  y:100,
-  opacity:0, 
-  delay:0.5, 
-  duration:0.9, 
-  stagger:0.3
-})
+  y: 100,
+  opacity: 0,
+  delay: 0.5,
+  duration: 0.9,
+  stagger: 0.3,
+});
 gsap.from(sectionTwoAnimation, {
-  y:100,
-  opacity:0, 
-  delay:1, 
-  duration:0.9, 
-  stagger:0.3
-})
-
-// section - 3 left panel position sticky java script code
+  y: 100,
+  opacity: 0,
+  delay: 1,
+  duration: 0.9,
+  stagger: 0.3,
+});
 
 
-    // Get references to the sidebar links and the right panel
-    const sidebarLinks = document.querySelectorAll('.sidebar-link');
-    const rightPanel = document.querySelector('.right-panel');
-
-    // Scroll event listener to handle the scrolling effect
-    rightPanel.addEventListener('scroll', () => {
-        sidebarLinks.forEach(link => {
-            const contentSection = document.getElementById(link.id.replace('-link', ''));
-            const contentRect = contentSection.getBoundingClientRect();
-            if (contentRect.top <= 150 && contentRect.bottom >= 150) {
-                link.classList.add('active');
-            } else {
-                link.classList.remove('active');
-            }
-        });
-    });
-
-
-    // highlight a specific section on scroll
-
- 
+// highlight a specific section on scroll
